15b287d95577e76a12a2fb3910f12587ab86b2a2
Haomeng <69145190+FZFZFZZ@users.noreply.github.com>
2025-10-16 22:15:45 +0800

    Build bug fixing

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -36,7 +36,7 @@ public class AddCommand extends Command {
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney "
-            + PREFIX_TIMESLOT + "2025-10-16 1700-1800";
+            + PREFIX_TIMESLOT + "2025-10-16 1500-1800";
 
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
